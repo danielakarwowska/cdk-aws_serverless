ff7405ef566eac0c4ce04e54fec2c3d3250040af
Daniela Kamola <daniela.karwowska@o2.pl>
2023-02-06 11:21:28 +0100

    update in package.json

--- cdk-serverless/lib/cdk-serverless-stack.ts
+++ cdk-serverless/lib/cdk-serverless-stack.ts
@@ -1,19 +1,15 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from '@aws-cdk/core'
+import * as cdk from '@aws-cdk/core'
+import * as lambda from "@aws-cdk/aws-lambda"
+import * as dynamodb from "@aws-cdk/aws-dynamodb"
+import * as apigateway from "@aws-cdk/aws-apigateway"
 
 export class CdkServerlessStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkServerlessQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkServerlessTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const table = new dynamodb.Table(this, "Hello", {
+      partitionKey: { name: "name", type: dynamodb.AttributeType.STRING }
+    })
   }
 }
